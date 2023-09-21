f72b9d141f5464fa55a51fa63b4e23a7c8a82650
casj <casper_jellesen@hotmailcom>
2023-09-21 20:34:06 +0200

    Think I made pathing work

--- main/main.go
+++ main/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/cjellesen/go-dsa/collections/stack/linked_stack"
 )
 
 func main() {
 	fmt.Println("Testing imprt")
-
+	asd := linked_stack.New()
+	asd.IsEmpty()
 	// fmt.Println("Is the stack empty: ", linked_stack.New().IsEmpty())
 }
