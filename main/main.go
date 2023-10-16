2911d0efc97d6efc6c6e244750ee3961afce7cff
casj <casj@apx10.com>
2023-10-16 17:22:40 +0200

    Removed none-running code part

--- main/main.go
+++ main/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/cjellesen/go-dsa/collections/stack/linked_stack"
 )
 
 func main() {
 	fmt.Println("Testing imprt")
-	fmt.Println("Is the stack empty: ", linked_stack.New().IsEmpty())
 }
